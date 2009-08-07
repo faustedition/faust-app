79f938a14cfec53f49f194efc4f03e513b519bb2
Gregor Middell <gregor@middell.net>
2009-08-07 09:55:09 +0000

    Share project "faust-server" into "https://intern.faustedition.net/svn"

---
+++ faust-server/src/main/java/de/faustedition/server/JettyServer.java
@@ -0,0 +1,43 @@
+package de.faustedition.server;
+
+import java.io.File;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import org.mortbay.jetty.Server;
+import org.mortbay.jetty.nio.SelectChannelConnector;
+import org.mortbay.jetty.webapp.WebAppContext;
+
+public class JettyServer {
+	private static final Logger logger = Logger.getLogger("de.faustedition");
+
+	public static void main(String[] args) {
+		try {
+			String faustWebapp = System.getProperty("faust.webapp");
+			if (faustWebapp == null) {
+				logger.severe("No system property 'faust.webapp' given");
+				System.exit(-1);
+			}
+			
+			File faustWebappFile = new File(faustWebapp);
+			if (!faustWebappFile.canRead()) {
+				logger.severe(String.format("Cannot read webapp source '%s'", faustWebappFile.getAbsolutePath()));
+				System.exit(-2);
+			}
+			
+			SelectChannelConnector connector = new SelectChannelConnector();
+			connector.setPort(9999);
+			connector.setForwarded(true);
+
+			final Server server = new Server();
+			server.addConnector(connector);
+			server.setHandler(new WebAppContext(faustWebappFile.toURI().toString(), "/"));
+			server.setStopAtShutdown(true);
+			server.start();
+			server.join();
+		} catch (Exception e) {
+			logger.log(Level.SEVERE, "Error starting Jetty server", e);
+			System.exit(-3);
+		}
+	}
+}
