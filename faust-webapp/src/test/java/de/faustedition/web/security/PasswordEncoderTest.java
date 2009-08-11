875a8a2216110663e9e1b53f4b8e8d4b662090d3
Gregor Middell <gregor@middell.net>
2009-08-11 07:03:47 +0000

    password protection for WebDAV access

---
+++ faust-webapp/src/test/java/de/faustedition/web/security/PasswordEncoderTest.java
@@ -0,0 +1,17 @@
+package de.faustedition.web.security;
+
+import org.apache.commons.codec.digest.DigestUtils;
+import org.junit.Test;
+
+import de.faustedition.util.LoggingUtil;
+
+public class PasswordEncoderTest {
+	public static final String[] PASSWORDS = new String[] { "hallo", "welt" };
+	
+	@Test
+	public void encode() {
+		for (String password : PASSWORDS) {
+			LoggingUtil.LOG.info(String.format("%s ==> %s", password, DigestUtils.shaHex(password)));
+		}
+	}
+}
