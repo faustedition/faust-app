efff2f5d46e51ecdd5893fe8b334ac909e0ffe0d
Gregor Middell <gregor@middell.net>
2010-09-08 16:11:06 -0400

    validation tool finished

--- faust-utils/faust.py
+++ faust-utils/faust.py
@@ -1,11 +1,21 @@
 #!/usr/bin/env python
 
 import ConfigParser
+import email.mime.text
 import os
 import os.path
 
+import lxml.etree
+
 config = ConfigParser.ConfigParser()
 config.read(['faust.ini', "local.ini"])
+
+report_sender = "Faust-Edition <noreply@faustedition.net>"
+report_recipients = ["Gregor Middell <gregor@middell.net>"]
+
+def relative_path(xml_file):
+	"""Returns the path of the given XML file relative to the base directory"""
+	return os.path.relpath(xml_file, config.get("xml", "dir"))
 
 def xml_files():
 	"""Returns paths of all XML documents in the edition"""
@@ -13,5 +23,27 @@ def xml_files():
 	for root, dirs, files in os.walk(config.get("xml", "dir")):		
 		for f in files: 
 			if f.endswith(".xml"): xml_files.append(os.path.join(root, f))
+	xml_files.sort()
 	return xml_files
 
+def is_tei_document(xml_file):
+	"""Determines whether a XML file is a TEI document by checking the namespace of the first element encountered"""
+	for event, element in lxml.etree.iterparse(xml_file):
+		if element is None: continue
+		return element.tag.startswith("{http://www.tei-c.org/ns/1.0}")
+
+def send_report(subject, msg):
+	if config.getboolean("mail", "enabled"):
+		msg = email.mime.text.MIMEText(msg)
+		msg["Subject"] = subject
+		msg["From"] = report_sender
+		msg["To"] = ", ".join(report_recipients)
+		
+		server = smtplib.SMTP('localhost')
+		server.sendmail(report_sender, report_recipients, msg.as_string())
+		server.quit()
+	else:
+		print "Subject:", subject
+		print
+		print msg
+	
--- faust-utils/validate.py
+++ faust-utils/validate.py
@@ -1,14 +1,52 @@
 #!/usr/bin/env python
 
+import re
+import shlex
+import subprocess
 import lxml.etree
 import faust
 
-schema = lxml.etree.parse(faust.config.get("xml", "schema"))
-print "Schema parsed"
+validation_command = "java -jar '%s' '%s'" % (faust.config.get("validate", "jing"), faust.config.get("validate", "schema"))
+validation_msg_re = re.compile(r'^(.+?):(\d+):(\d+):(.+?)$', re.MULTILINE)
 
-schema_validator = lxml.etree.RelaxNG(schema)
-print "Schema validator built"
+validation_queue_max_length = 100
+validation_queue = []
 
+validation_report = dict()
+
+def validate(last=False):
+	"""Validates the content of the queue by calling Jing and parsing its output"""
+	global validation_queue
+	if not last and len(validation_queue) < validation_queue_max_length: return
+
+	validation = subprocess.Popen(shlex.split(validation_command) + validation_queue, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+	validation_result = validation.communicate()[0] or ""
+	for msg_match in validation_msg_re.finditer(validation_result):
+		xml_file, line, column, msg = msg_match.groups()
+		
+		xml_file = faust.relative_path(xml_file)
+		error_msg = "[%s:%s] %s" % (line, column, msg)
+		
+		if xml_file in validation_report: 
+			validation_report[xml_file].append(error_msg)
+		else:
+			validation_report[xml_file] = [error_msg]
+
+	validation_queue = []
+	
+# Validate all XML documents
 for xml_file in faust.xml_files():
-	print xml_file
-	lxml.etree.parse(xml_file)
+	if faust.is_tei_document(xml_file):
+		validation_queue.append(xml_file)
+		validate()
+validate(True)
+
+# Generate validation report
+if len(validation_report) > 0:
+	report = ""
+	xml_files = validation_report.keys()
+	xml_files.sort()
+	for xml_file in xml_files:
+		report += ("== " + xml_file + " ==\n\n")
+		report += "\n".join(validation_report[xml_file]) + "\n\n"
+	faust.send_report("TEI-P5 Validator Report", report)
