02287f9c963bda3d51b09cc969dd65f4ca4a3957
Gregor Middell <gregor@middell.net>
2012-04-18 17:51:11 +0200

    Restlet update

--- faust/src/main/java/de/faustedition/Runtime.java
+++ faust/src/main/java/de/faustedition/Runtime.java
@@ -1,5 +1,7 @@
 package de.faustedition;
 
+import de.faustedition.inject.FaustInjector;
+
 import java.io.File;
 import java.io.PrintWriter;
 import java.io.StringWriter;
@@ -8,17 +10,7 @@ import java.util.Enumeration;
 import java.util.Locale;
 import java.util.SortedSet;
 import java.util.TreeSet;
-import java.util.logging.ConsoleHandler;
-import java.util.logging.Formatter;
-import java.util.logging.Handler;
-import java.util.logging.Level;
-import java.util.logging.LogManager;
-import java.util.logging.LogRecord;
-import java.util.logging.Logger;
-
-import org.eclipse.jetty.util.log.Log;
-
-import de.faustedition.inject.FaustInjector;
+import java.util.logging.*;
 
 public abstract class Runtime {
 	protected static Logger log = Logger.getLogger(Runtime.class.getName());
@@ -68,7 +60,6 @@ public abstract class Runtime {
 			logger.setLevel(Level.WARNING);
 
 		}
-		Log.setLog(new JettyRedirectingLogger());
 	}
 
 	public static void dumpLogConfig() {
@@ -105,73 +96,5 @@ public abstract class Runtime {
 			return msg.toString();
 		}
 
-	}
-
-	private static class JettyRedirectingLogger implements org.eclipse.jetty.util.log.Logger {
-		private final Logger logger;
-
-		private JettyRedirectingLogger() {
-			this.logger = Logger.getLogger("org.eclipse.jetty");
-		}
-
-		@Override
-		public boolean isDebugEnabled() {
-			return logger.isLoggable(Level.FINE);
-		}
-
-		@Override
-		public void setDebugEnabled(boolean enabled) {
-		}
-
-		@Override
-		public void info(String msg) {
-			logger.info(msg);
-		}
-
-		@Override
-		public void info(String msg, Object arg0, Object arg1) {
-			logger.log(Level.INFO, msg, new Object[] { arg0, arg1 });
-		}
-
-		@Override
-		public void debug(String msg) {
-			logger.fine(msg);
-		}
-
-		@Override
-		public void debug(String msg, Throwable th) {
-			logger.log(Level.FINE, msg, th);
-
-		}
-
-		@Override
-		public void debug(String msg, Object arg0, Object arg1) {
-			logger.log(Level.FINE, msg, new Object[] { arg0, arg1 });
-		}
-
-		@Override
-		public void warn(String msg) {
-			logger.warning(msg);
-		}
-
-		@Override
-		public void warn(String msg, Object arg0, Object arg1) {
-			logger.log(Level.WARNING, msg, new Object[] { arg0, arg1 });
-		}
-
-		@Override
-		public void warn(String msg, Throwable th) {
-			logger.log(Level.WARNING, msg, th);
-		}
-
-		@Override
-		public org.eclipse.jetty.util.log.Logger getLogger(String name) {
-			return new JettyRedirectingLogger();
-		}
-
-		@Override
-		public String getName() {
-			return logger.getName();
-		}
 	}
 }
