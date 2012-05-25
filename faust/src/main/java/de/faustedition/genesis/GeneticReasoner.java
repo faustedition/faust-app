2f2472b8d63afc6e6f1855bf7d0d81b18cb2beb0
moz <moritz@wissenba.ch>
2012-05-25 16:44:08 +0200

    next iteration of genetic reasoning

--- faust/src/main/java/de/faustedition/genesis/GeneticReasoner.java
+++ faust/src/main/java/de/faustedition/genesis/GeneticReasoner.java
@@ -1,24 +1,115 @@
 package de.faustedition.genesis;
 
+import java.io.IOException;
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.xml.stream.XMLStreamException;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.TransactionStatus;
+import org.springframework.transaction.support.TransactionCallback;
+import org.springframework.transaction.support.TransactionTemplate;
 
+import de.faustedition.FaustURI;
+import de.faustedition.Runtime;
+import de.faustedition.dataimport.DataImport;
+import de.faustedition.document.Document;
+import de.faustedition.document.MaterialUnit;
+import de.faustedition.graph.FaustGraph;
 import de.faustedition.reasoning.GeneticReasoning;
+import de.faustedition.reasoning.Inscription;
+import de.faustedition.transcript.Transcript;
 import de.faustedition.xml.XMLStorage;
 
-
 @Component
-public class GeneticReasoner {
-	
+public class GeneticReasoner extends Runtime implements Runnable {
+
 	@Autowired
-	private static XMLStorage xml;
-	
-	
-	public static void reason() {
-		
-		GeneticReasoning r = new GeneticReasoning();		
-		// generate inscriptions...
-		//r.reason();
-		
+	private XMLStorage xml;
+
+	@Autowired
+	private FaustGraph graph;
+
+	@Autowired
+	private SessionFactory sessionFactory;
+
+	@Autowired
+	private TransactionTemplate tt;
+
+	public static void main(String[] args) throws Exception {
+		try {
+			main(GeneticReasoner.class, args);
+		} finally {
+			System.exit(0);
+		}
+	}
+
+	private static final Logger LOG = LoggerFactory
+	.getLogger(GeneticReasoner.class);
+
+	@Override
+	public void run() {
+
+		final Set<Inscription> inscriptions = inscriptions();
+		final GeneticReasoning reasoning = new GeneticReasoning();
+		LOG.debug("Reasoning on " + inscriptions.size() + " inscriptions");
+		for (Inscription i: inscriptions)
+			LOG.debug("Inscription from " + (i.first()) + " to " +
+					i.last());
+		reasoning.reason(inscriptions);
+
+	}
+
+	private HashSet<Inscription> inscriptions() {		
+		final HashSet<Inscription> inscriptions = new HashSet<Inscription>();
+
+		try {
+			for (MaterialUnit mu : graph.getMaterialUnits()) {
+				if (mu instanceof Document) {
+					final Document document = (Document) mu;
+					final FaustURI source = document.getSource();
+
+					Transcript transcript = tt
+					.execute(new TransactionCallback<Transcript>() {
+						@Override
+						public Transcript doInTransaction(
+								TransactionStatus status) {
+							Session session = sessionFactory
+							.getCurrentSession();
+							try {
+								Transcript transcript = Transcript
+								.find(session, xml, source);
+								TranscribedVerseInterval.register(
+										session, transcript);
+								Iterable<TranscribedVerseInterval> intervals = TranscribedVerseInterval
+								.registeredFor(session,
+										transcript);
+								Inscription inscription = new Inscription();
+								for (TranscribedVerseInterval interval : intervals) {
+									inscription.addInterval(interval.getStart(), interval.getEnd());									
+								}
+								inscriptions.add(inscription);
+							} catch (IOException e) {
+								LOG.error("IO error in " + source, e);
+							} catch (XMLStreamException e) {
+								LOG.error("XML error in " + source, e);
+							}
+
+							return null;
+						}
+					});
+				}
+
+			}
+		} catch (Exception e) {
+			System.out.println(e);
+		}
+		return inscriptions;
 	}
 }
--- genetic-reasoning/src/main/java/de/faustedition/reasoning/Inscription.java
+++ genetic-reasoning/src/main/java/de/faustedition/reasoning/Inscription.java
@@ -10,7 +10,17 @@ public class Inscription extends TreeSet<Integer> {
 	 */
 	private static final long serialVersionUID = 1L;
 	
+	public Inscription() {
+		super();
+	}
+	
 	public Inscription(Collection<? extends Integer> c) {
 		super(c);
+	}
+	
+	public void addInterval(int from, int to) {
+		for (int i = from; i<=to; i++) {
+			this.add(i);
+		}
 	}
 }
