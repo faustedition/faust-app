792bb1cc27326477778f989a14802eaba5c6df5b
Gregor Middell <gregor@middell.net>
2012-05-29 14:09:20 +0100

    parent link in tree nodes

--- faust/src/main/static/js/faust/text-annotation/text-index.js
+++ faust/src/main/static/js/faust/text-annotation/text-index.js
@@ -23,7 +23,6 @@
 YUI.add('text-index', function (Y) {
     var Iterator = function (tree) {
         this._tree = tree;
-        this._ancestors = [];
         this._cursor = null;
     };
     Y.extend(Iterator, Object, {
@@ -45,16 +44,13 @@ YUI.add('text-index', function (Y) {
                     var save;
                     do {
                         save = this._cursor;
-                        if (this._ancestors.length) {
-                            this._cursor = this._ancestors.pop();
-                        } else {
-                            this._cursor = null;
+                        this._cursor = this._cursor.parent;
+                        if (this._cursor === null) {
                             break;
                         }
                     } while (this._cursor.right === save);
                 } else {
                     // get the next node from the subtree
-                    this._ancestors.push(this._cursor);
                     this._minNode(this._cursor.right);
                 }
             }
@@ -74,15 +70,12 @@ YUI.add('text-index', function (Y) {
                     var save;
                     do {
                         save = this._cursor;
-                        if (this._ancestors.length) {
-                            this._cursor = this._ancestors.pop();
-                        } else {
-                            this._cursor = null;
+                        this._cursor = this._cursor.parent;
+                        if (this._cursor === null) {
                             break;
                         }
                     } while (this._cursor.left === save);
                 } else {
-                    this._ancestors.push(this._cursor);
                     this._maxNode(this._cursor.left);
                 }
             }
@@ -90,14 +83,12 @@ YUI.add('text-index', function (Y) {
         },
         _minNode: function (start) {
             while (start.left !== null) {
-                this._ancestors.push(start);
                 start = start.left;
             }
             this._cursor = start;
         },
         _maxNode: function (start) {
             while (start.right !== null) {
-                this._ancestors.push(start);
                 start = start.right;
             }
             this._cursor = start;
@@ -157,14 +148,14 @@ YUI.add('text-index', function (Y) {
         iterator: function () {
             return new Iterator(this);
         },
-        // calls cb on each node's data, in order
+        // calls cb on each node, in order
         each: function (cb) {
             var it = this.iterator(), node;
             while ((node = it.next()) !== null) {
                 cb(node);
             }
         },
-        // calls cb on each node's data, in reverse order
+        // calls cb on each node, in reverse order
         reach: function (cb) {
             var it = this.iterator(), node;
             while ((node = it.prev()) !== null) {
@@ -176,6 +167,7 @@ YUI.add('text-index', function (Y) {
     var Node = function (key) {
         this.key = key;
         this.values = [];
+        this.parent = null;
         this.left = null;
         this.right = null;
         this.red = true;
@@ -189,6 +181,9 @@ YUI.add('text-index', function (Y) {
                 this.right = val;
             } else {
                 this.left = val;
+            }
+            if (val !== null) {
+                val.parent = this;
             }
         }
     });
