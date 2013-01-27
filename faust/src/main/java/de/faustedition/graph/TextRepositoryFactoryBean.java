1e9fba2b238527a5a6a83255b1c5faa421e3378f
gregor <gregor@middell.net>
2013-01-27 20:30:59 +0000

    Instantiate a Neo4j-backed text repository

---
+++ faust/src/main/java/de/faustedition/graph/TextRepositoryFactoryBean.java
@@ -0,0 +1,33 @@
+package de.faustedition.graph;
+
+import eu.interedition.text.TextRepository;
+import eu.interedition.text.json.JacksonDataNodeMapper;
+import eu.interedition.text.neo4j.Neo4jTextRepository;
+import org.codehaus.jackson.JsonNode;
+import org.codehaus.jackson.map.ObjectMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.config.AbstractFactoryBean;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author <a href="http://gregor.middell.net/" title="Homepage">Gregor Middell</a>
+ */
+@Component
+public class TextRepositoryFactoryBean extends AbstractFactoryBean<TextRepository<JsonNode>> {
+
+  @Autowired
+  private FaustGraph faustGraph;
+
+  @Autowired
+  private ObjectMapper objectMapper;
+
+  @Override
+  public Class<?> getObjectType() {
+    return TextRepository.class;
+  }
+
+  @Override
+  protected TextRepository<JsonNode> createInstance() throws Exception {
+    return new Neo4jTextRepository<JsonNode>(JsonNode.class, new JacksonDataNodeMapper<JsonNode>(objectMapper), faustGraph.getDb(), false);
+  }
+}
