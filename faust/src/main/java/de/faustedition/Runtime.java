16d0436d83be0f8536b354618f389cb72d7bcd30
gremid <gregor@middell.net>
2013-05-04 10:05:49 +0100

    Add config file lookup in default locations

--- faust/src/main/java/de/faustedition/Runtime.java
+++ faust/src/main/java/de/faustedition/Runtime.java
@@ -3,10 +3,12 @@ package de.faustedition;
 import java.io.File;
 import java.io.IOException;
 import java.util.Arrays;
+import java.util.List;
 import java.util.Locale;
 import java.util.logging.ConsoleHandler;
 import java.util.logging.Logger;
 
+import com.google.common.collect.Lists;
 import org.slf4j.bridge.SLF4JBridgeHandler;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.core.env.MutablePropertySources;
@@ -18,15 +20,21 @@ import com.google.common.collect.Iterables;
 
 public abstract class Runtime {
 
+    private static final String CONFIG_FILE_NAME = "faust.properties";
+
 	public static void main(Class<? extends Runnable> clazz, String[] args) throws IOException {
 		Locale.setDefault(new Locale("en", "us"));
-		
-		File configFile = null;
+
+		final List<File> configFileCandidates = Lists.newLinkedList(Arrays.asList(
+                new File(System.getProperty("user.home"), CONFIG_FILE_NAME),
+                new File(System.getProperty("user.dir"), CONFIG_FILE_NAME)
+        ));
 		for (String arg : args) {
 			if ("-debug".equalsIgnoreCase(arg)) {
 				continue;
-			} else if (configFile == null) {
-				configFile = new File(arg);
+			} else {
+				configFileCandidates.add(new File(arg));
+                break;
 			}
 		}
 
@@ -40,9 +48,11 @@ public abstract class Runtime {
 
 		final MutablePropertySources ps = applicationContext.getEnvironment().getPropertySources();
 		ps.addLast(new PropertiesPropertySource("system", System.getProperties()));
-		if (configFile != null) {
-			ps.addLast(new ResourcePropertySource(new FileSystemResource(configFile)));
-		}
+        for (File configFileCandidate : configFileCandidates) {
+            if (configFileCandidate.isFile()) {
+                ps.addLast(new ResourcePropertySource(new FileSystemResource(configFileCandidate)));
+            }
+        }
 		ps.addLast(new ResourcePropertySource("classpath:/config-default.properties"));
 
 		applicationContext.registerShutdownHook();
