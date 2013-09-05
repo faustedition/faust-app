f903790702527cb970d2fc1cc073a4fdbf02e382
Malte Renken <malte.renken@wwu.de>
2013-09-05 16:04:44 +0200

    Added special CSS classes first and last and speakernumber. Added example CSS stylesheet.

--- faust/src/main/static/js/faust/text-display/text-display.js
+++ faust/src/main/static/js/faust/text-display/text-display.js
@@ -7,17 +7,37 @@ YUI.add('text-display', function (Y) {
 		render : function() {
 			var text = this.get('text');
 			var container = this.get('container');
+			var prefix = this.get('cssPrefix');
 
 			var partitions = text.applyAnnotations();
 			
-			Y.Array.each(partitions, function(partition){
+			function stageNum(name){
+				stageNum.stages = stageNum.stages || {};
+				stageNum.stagecount = stageNum.stagecount || 0;
+				if(stageNum.stages[name] === undefined)
+					stageNum.stages[name] = stageNum.stagecount++;
+				return stageNum.stages[name];
+			}
+			
+			Y.Array.each(partitions, function(partition, i, partitions){
 				var partitionNode = Y.Node.create('<span></span>')
 				container.append(partitionNode);
 				var content = partition.range.of(text.content);
 				content = content.replace('\n', '<br/>');
 				partitionNode.setHTML(content); // fixme: this swallows whitespace
 				Y.Array.each(partition.annotations, function(annotation){
-					partitionNode.addClass(annotation.name.localName);
+					
+					var name = prefix + annotation.name.localName;
+					
+					partitionNode.addClass(name);
+					
+					if(i == 0 || Y.Array.indexOf(partitions[i-1].annotations, annotation) == -1)
+						partitionNode.addClass(name + '-first');
+					if(i+1 == partitions.length || Y.Array.indexOf(partitions[i+1].annotations, annotation) == -1)
+						partitionNode.addClass(name + '-last');
+					
+					if(annotation.name.localName == 'stage')
+						partitionNode.addClass(name + '-' + stageNum(annotation.data['value']));
 				});
 			});
 		}
@@ -25,6 +45,7 @@ YUI.add('text-display', function (Y) {
 		ATTRS: {
 			container: {}, /// reference to HTML node acting as output container
 			text: {}, /// instance of Y.Faust.Text
+			cssPrefix: {value: ""}, /// CSS prefix string
 		}
 	});
 	
