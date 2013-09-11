c051398ceee179c394a1c0401be51ee4b04410c3
Malte Renken <malte.renken@wwu.de>
2013-09-11 23:48:59 +0200

    Added new module to yui-config.

--- faust/src/main/static/js/yui-config.js
+++ faust/src/main/static/js/yui-config.js
@@ -1,21 +1,21 @@
 YUI.GlobalConfig = {
-    debug: false,
-    combine: false,
-    comboBase: cp + '/resources?',
-    root: 'yui3/build/',
-    groups: {
-        faust: {
-            base: cp + '/static/js/',
-            combine: false,
-            comboBase: cp + '/resources?',
-            filter: "raw",
-            root: 'js/',
-            modules: {
-                'adhoc-tree': { path: 'faust/text-annotation/adhoc-tree.js'},
-                'facsimile': { path: 'faust/facsimile/facsimile.js'},
-                'facsimile-svgpane': { path: 'faust/facsimile/facsimile-svgpane.js' },
-                'facsimile-highlightpane': { path: 'faust/facsimile/facsimile-highlightpane.js' },
-                'util': { path: "faust/util/util.js"},
+	debug: false,
+	combine: false,
+	comboBase: cp + '/resources?',
+	root: 'yui3/build/',
+	groups: {
+		faust: {
+			base: cp + '/static/js/',
+			combine: false,
+			comboBase: cp + '/resources?',
+			filter: "raw",
+			root: 'js/',
+			modules: {
+				'adhoc-tree': { path: 'faust/text-annotation/adhoc-tree.js'},
+				'facsimile': { path: 'faust/facsimile/facsimile.js'},
+				'facsimile-svgpane': { path: 'faust/facsimile/facsimile-svgpane.js' },
+				'facsimile-highlightpane': { path: 'faust/facsimile/facsimile-highlightpane.js' },
+				'util': { path: "faust/util/util.js"},
 				'document-adhoc-tree' : { path: 'faust/document/document-adhoc-tree.js' },
 				'document-configuration-faust' : { path: 'faust/document/document-configuration-faust.js' },
 				'document-yui-view' : { path: 'faust/document/document-yui-view.js' },
@@ -24,13 +24,14 @@ YUI.GlobalConfig = {
 				'document-model' : { path: 'faust/document/document-model.js' },
 				'document-text' : { path: 'faust/document/document-text.js' },
 				'materialunit' : { path: 'faust/document/materialunit.js' },
-                'protovis': { path: "protovis-r3.2.js"},
-                'search': { path: "faust/search/search.js"},
+				'protovis': { path: "protovis-r3.2.js"},
+				'search': { path: "faust/search/search.js"},
 				'svg-utils' : { path: "faust/svg-utils/svg-utils.js"},
-                'text-annotation': { path: "faust/text-annotation/text-annotation.js"},
+				'text-annotation': { path: "faust/text-annotation/text-annotation.js"},
 				'text-display': { path: "faust/text-display/text-display.js"},
-                'text-index': { path: 'faust/text-annotation/text-index.js'}
-            }
-        }
-    }
+				'paged-text-display': { path: 'faust/text-display/paged-text-display.js'},
+				'text-index': { path: 'faust/text-annotation/text-index.js'}
+			}
+		}
+	}
 };
