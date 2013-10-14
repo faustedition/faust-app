51817eefd115fc6c588312be3a0cf298af8f781e
gremid <gregor@middell.net>
2013-10-14 10:23:51 +0100

    Data ports for archives and documents

--- faust/src/main/java/de/faustedition/document/ArchiveResource.java
+++ faust/src/main/java/de/faustedition/document/ArchiveResource.java
@@ -11,9 +11,14 @@ import javax.inject.Singleton;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.WebApplicationException;
 import javax.ws.rs.core.Context;
+import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Request;
 import javax.ws.rs.core.Response;
+import java.util.List;
+import java.util.Map;
 
 @Path("/archive")
 @Singleton
@@ -29,38 +34,59 @@ public class ArchiveResource {
     }
 
     @GET
-    public Response index(@Context final Request request) {
+    public Response indexView(@Context final Request request) {
         return database.transaction(new Database.TransactionCallback<Response>() {
             @Override
             public Response doInTransaction(DSLContext sql) throws Exception {
-                return templates.render(request, new Templates.ViewAndModel("document/archives")
-                        .add("archives", sql.selectFrom(Tables.ARCHIVE).orderBy(Tables.ARCHIVE.NAME.asc()).fetchMaps())
-                );
+                return templates.render(request, new Templates.ViewAndModel("document/archives").add("archives", index()));
+            }
+        });
+    }
+
+    @GET
+    @Path("/data")
+    @Produces(MediaType.APPLICATION_JSON)
+    public List<Map<String, Object>> index() {
+        return database.transaction(new Database.TransactionCallback<List<Map<String,Object>>>() {
+            @Override
+            public List<Map<String, Object>> doInTransaction(DSLContext sql) throws Exception {
+                return sql.selectFrom(Tables.ARCHIVE).orderBy(Tables.ARCHIVE.NAME.asc()).fetchMaps();
             }
         });
     }
 
     @Path("/{id}")
     @GET
-    public Response archive(@PathParam("id") final String id, @Context final Request request) {
-        return database.transaction(new Database.TransactionCallback<Response>() {
+    public Response archiveView(@PathParam("id") final String id, @Context final Request request) {
+        return templates.render(request, archive(id));
+    }
+
+    @GET
+    @Path("/{id}/data")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Templates.ViewAndModel archive(@PathParam("id") final String id) {
+        return database.transaction(new Database.TransactionCallback<Templates.ViewAndModel>() {
             @Override
-            public Response doInTransaction(DSLContext sql) throws Exception {
+            public Templates.ViewAndModel doInTransaction(DSLContext sql) throws Exception {
                 final ArchiveRecord archive = sql.selectFrom(Tables.ARCHIVE).where(Tables.ARCHIVE.LABEL.eq(id)).fetchOne();
                 if (archive == null) {
-                    return Response.status(Response.Status.NOT_FOUND).entity(id).build();
+                    throw new WebApplicationException(Response.status(Response.Status.NOT_FOUND).entity(id).build());
                 }
 
-                return templates.render(request, new Templates.ViewAndModel("document/archive")
-                        .add("archive", archive.intoMap())
-                        .add("documents", sql
-                                .select(Tables.DOCUMENT.ID, Tables.DOCUMENT.CALLNUMBER, Tables.DOCUMENT.WA_ID)
-                                .from(Tables.DOCUMENT)
-                                .where(Tables.DOCUMENT.ARCHIVE_ID.eq(archive.getId()))
-                                .orderBy(Tables.DOCUMENT.CALLNUMBER.asc())
-                                .fetchMaps()
-                        )
+                return new Templates.ViewAndModel("document/archive")
+                    .add("archive", archive.intoMap())
+                    .add("documents", sql
+                            .select(Tables.DOCUMENT.ID, Tables.DOCUMENT.CALLNUMBER, Tables.DOCUMENT.WA_ID)
+                            .from(Tables.DOCUMENT)
+                            .where(Tables.DOCUMENT.ARCHIVE_ID.eq(archive.getId()))
+                            .orderBy(Tables.DOCUMENT.CALLNUMBER.asc())
+                            .fetchMaps()
                 );
+            }
+
+            @Override
+            protected boolean rollsBackOn(Exception e) {
+                return !(e instanceof WebApplicationException);
             }
         });
     }
--- faust/src/main/java/de/faustedition/document/DocumentResource.java
+++ faust/src/main/java/de/faustedition/document/DocumentResource.java
@@ -1,116 +1,102 @@
 package de.faustedition.document;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ArrayNode;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import de.faustedition.Database;
 import de.faustedition.Templates;
+import de.faustedition.db.Tables;
+import org.jooq.DSLContext;
+import org.jooq.Record1;
+import org.jooq.Record3;
+import org.jooq.Result;
 
 import javax.inject.Inject;
 import javax.inject.Singleton;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.WebApplicationException;
+import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Request;
 import javax.ws.rs.core.Response;
-import java.io.IOException;
 
 
 @Path("/document")
 @Singleton
 public class DocumentResource {
 
+    private final Database database;
     private final Templates templates;
+    private final ObjectMapper objectMapper;
 
     @Inject
-    public DocumentResource(Templates templates) {
+    public DocumentResource(Database database, Templates templates, ObjectMapper objectMapper) {
+        this.database = database;
         this.templates = templates;
+        this.objectMapper = objectMapper;
     }
 
+    @GET
+    @Path("/{id}")
+    public Response documentView(@Context Request request, @PathParam("id") final long id) {
+        return templates.render(request, document(id));
+    }
 
 	@GET
-    @Produces(MediaType.TEXT_HTML)
-	public Response overview() throws IOException {
-        /*
-		Map<String, Object> viewModel = new HashMap<String, Object>();
-		viewModel.put("document", document);
-		viewModel.put("contents", document.getSortedContents());
-		viewModel.put("path", path.toString());
-
-		Map<String, String> params = this.getRequest().getResourceRef().getQueryAsForm().getValuesMap();
-		if ("transcript-bare".equals(params.get("view"))) {
-			return viewFactory.create("document/document-transcript-bare", getRequest().getClientInfo(), viewModel);
-		} else {				
-			return viewFactory.create("document/document-app", getRequest().getClientInfo(), viewModel);
-		}
-		*/
-        return null;
-	}
-
-    @GET
-    @Produces(DocumentImageLinks.IMAGE_SVG_TYPE)
-	public Response graphic() {
-		String result = "<xml version=\"1open.0\"?><svg xmlns=\"http://www.w3.org/2000/svg\" version=\"1.1\">"
-			+ "<script type=\"text/javascript\" ></script>"
-			+ "</svg>";
-		
-		/*
-		return new StringRepresentation(result, MediaType.IMAGE_SVG,
-				Language.DEFAULT, CharacterSet.UTF_8);
-        */
-        return Response.ok(result).build();
-	}
-
-    @GET
+    @Path("/{id}/data")
     @Produces(MediaType.APPLICATION_JSON)
-	public Response documentStructure() {
-        /*
-		return new JsonRespresentation() {
+	public Templates.ViewAndModel document(@PathParam("id") final long id) {
+        return database.transaction(new Database.TransactionCallback<Templates.ViewAndModel>() {
+            @Override
+            public Templates.ViewAndModel doInTransaction(DSLContext sql) throws Exception {
+                final Record1<String> document = sql.select(Tables.DOCUMENT.METADATA)
+                        .from(Tables.DOCUMENT)
+                        .where(Tables.DOCUMENT.ID.eq(id))
+                        .fetchOne();
 
-			@Override
-			protected void generate() throws IOException {
-				generate(document);
-			}
+                if (document == null) {
+                    throw new WebApplicationException(Response.status(Response.Status.NOT_FOUND).entity(id).build());
+                }
 
-			protected void generate(MaterialUnit unit) throws IOException {
-				generator.writeStartObject();
+                final Result<Record3<Long,Long,String>> materialUnits = sql
+                        .select(Tables.MATERIAL_UNIT.ID, Tables.MATERIAL_UNIT.TRANSCRIPT_ID, Tables.FACSIMILE.PATH)
+                        .from(Tables.MATERIAL_UNIT)
+                        .leftOuterJoin(Tables.FACSIMILE)
+                        .on(Tables.FACSIMILE.MATERIAL_UNIT_ID.eq(Tables.MATERIAL_UNIT.ID))
+                        .where(Tables.MATERIAL_UNIT.DOCUMENT_ID.eq(id))
+                        .orderBy(Tables.MATERIAL_UNIT.DOCUMENT_ORDER.asc(), Tables.FACSIMILE.FACSIMILE_ORDER.asc())
+                        .fetch();
 
-				generator.writeStringField("type", unit.getType().name().toLowerCase());
-				if (unit instanceof Document) {
-					generator.writeStringField("name", unit.toString());
-				}
-				generator.writeNumberField("order", unit.getOrder());
-				generator.writeNumberField("id", unit.node.getId());
-//				final GoddagTranscript transcript = unit.getTranscript();
-				if (unit.getTranscriptSource() != null) {
-					generator.writeObjectFieldStart("transcript");
-//					generator.writeStringField("type", transcript.getType().name().toLowerCase());
-					generator.writeStringField("source", unit.getTranscriptSource().toString());
-//					if (transcript.getType() == TranscriptType.DOCUMENTARY) {
-//						DocumentaryGoddagTranscript dt = (DocumentaryGoddagTranscript) transcript;
-//						generator.writeArrayFieldStart("facsimiles");
-//						for (FaustURI facsimile : dt.getFacsimileReferences()) {
-//							generator.writeString(facsimile.toString());
-//						}
-//						generator.writeEndArray();
-//					}
-					generator.writeEndObject();
-					final FaustURI facsimile = unit.getFacsimile();
-					if (facsimile != null) 
-							generator.writeStringField("facsimile", facsimile.toString());
-					
+                final ArrayNode pages = objectMapper.createArrayNode();
+                ObjectNode materialUnit;
+                ArrayNode facsimiles = null;
+                long currentUnit = 0;
+                for (Record3<Long,Long,String> materialUnitData : materialUnits) {
+                    if (materialUnitData.value1() != currentUnit) {
+                        currentUnit = materialUnitData.value1();
+                        materialUnit = pages.addObject();
+                        facsimiles = materialUnit.putArray("facsimiles");
+                        if (materialUnitData.value2() != null) {
+                            materialUnit.put("transcript", materialUnitData.value2());
+                        }
+                    }
+                    if (materialUnitData.value3() != null) {
+                        facsimiles.add(materialUnitData.value3());
+                    }
+                }
 
+                return new Templates.ViewAndModel("document/document-app")
+                        .add("document", objectMapper.reader().readTree(document.value1()))
+                        .add("pages", pages);
+            }
 
-				}
-
-				generator.writeArrayFieldStart("contents");
-				for (MaterialUnit content : new TreeSet<MaterialUnit>(unit)) {
-					generate(content);
-				}
-				generator.writeEndArray();
-
-				generator.writeEndObject();
-			}
-		};
-		*/
-        return null;
+            @Override
+            protected boolean rollsBackOn(Exception e) {
+                return !(e instanceof WebApplicationException);
+            }
+        });
 	}
-
-	
 }
