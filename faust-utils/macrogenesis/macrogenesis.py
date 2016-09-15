c85cf8dc1fe13d0ef1b97fc38c44c444f7aff20a
moz <moritz@wissenba.ch>
2016-09-15 18:09:55 +0200

    cleaner separation of genetic data and visualisation

--- faust-utils/macrogenesis/macrogenesis.py
+++ faust-utils/macrogenesis/macrogenesis.py
@@ -1,8 +1,70 @@
+import pickle
 import networkx
 import lxml.etree as etree
 import logging
 import faust
 import datetime
+
+KEY_RELATION_NAME = 'relation_name'
+KEY_ABSOLUTE_DATINGS = 'absolute_datings'
+KEY_ABSOLUTE_DATINGS_PICKLED = 'absolute_datings_pickled'
+
+VALUE_IMPLICIT_FROM_ABSOLUTE = 'implicit_from_absolute'
+
+GENETIC_RELATION_MAP = {'temp-pre': 'temp-pre',
+                        'temp-syn': 'temp-syn'}
+
+
+class AbsoluteDating:
+    # None can be passed for missing values
+    def __init__(self, when, from_, to, not_before, not_after):
+        self.when = when
+        self.from_ = from_
+        self.to = to
+        self.not_before = not_before
+        self.not_after = not_after
+
+        if self.when is not None:
+            date_average = self.when
+        else:
+            if self.first_possible() is not None and self.last_possible() is None:
+                date_average = self.first_possible()
+            else:
+                if self.last_possible() is not None and self.first_possible() is None:
+                    date_average = self.last_possible()
+                else:
+                    if self.first_possible() is not None and self.last_possible() is not None:
+                        date_average = self.first_possible() + ((self.last_possible() - self.first_possible()) / 2)
+                    else:
+                        raise ValueError('Wrong combination of arguments for AbsoluteDating, or too few.')
+
+        self.average = date_average
+
+    def __str__(self):
+        return "{0}".format(format_date(self.when)) if self.when is not None else "{0} - {1}" \
+            .format(format_date(self.first_possible()), format_date(self.last_possible()), format_date(self.average))
+
+    # Returns not_before or, if not available, from date
+    def first_possible(self):
+        if (self.not_before is not None):
+            return self.not_before
+        else:
+            return self.from_
+
+    # Returns not_after or, if not available, to date
+    def last_possible(self):
+        if (self.not_after is not None):
+            return self.not_after
+        else:
+            return self.to
+
+
+# datetime.strftime cannot handle years before 1900, so we have to roll our own
+def format_date(date):
+    if date is None:
+        return '...'
+    else:
+        return '{0}.{1}.{2}'.format(date.day, date.month, date.year)
 
 
 def label_from_uri(uri):
@@ -22,55 +84,56 @@ def parse_datestr(datestr):
     return None if datestr is None else datetime.datetime.strptime(datestr, '%Y-%m-%d')
 
 
-# datetime.strftime cannot handle years before 1900, so we have to roll our own
-def format_date(date):
-    if date is None:
-        return '...'
-    else:
-        return '{0}.{1}.{2}'.format(date.day, date.month, date.year)
+def insert_minimal_edges_from_absolute_datings(graph):
+    # list of (node_id, node_attr) tuples
+    absolutely_dated_nodes = [(n, graph.node[n]) for n in graph.nodes() if KEY_ABSOLUTE_DATINGS in graph.node[n].keys()]
+    logging.debug("Sorting dates")
+    absolutely_dated_nodes.sort(key=lambda n: n[1][KEY_ABSOLUTE_DATINGS][0].average)
+    previous_dated_node = None
+    for node in absolutely_dated_nodes:
+        if previous_dated_node is not None:
+            graph.add_edge(previous_dated_node, node[0], attr_dict={KEY_RELATION_NAME: VALUE_IMPLICIT_FROM_ABSOLUTE})
+        previous_dated_node = node[0]
 
 
+# call parse_relationships() first to initialize graph
 def parse_dates(macrogenetic_document, graph):
     dates = macrogenetic_document.getroot().findall('f:date', namespaces=faust.namespaces)
 
     for (date_index, date) in enumerate(dates):
-        date_when = parse_datestr(date.attrib["when"]) if date.attrib.has_key("when") else None
 
-        # simplify and treat 'notBefore' and 'notAfter' like 'from' and 'to'
+        try:
 
-        date_from = parse_datestr(
-            date.attrib["from"] if date.attrib.has_key("from") else date.attrib["notBefore"] if date.attrib.has_key(
-                "notBefore") else None)
-        date_to = parse_datestr(
-            date.attrib["to"] if date.attrib.has_key("to") else date.attrib["notAfter"] if date.attrib.has_key(
-                "notAfter") else None)
+            absolute_dating = AbsoluteDating(
+                parse_datestr(date.attrib["when"] if date.attrib.has_key("when") else None),
+                parse_datestr(date.attrib["from"] if date.attrib.has_key("from") else None),
+                parse_datestr(date.attrib["to"] if date.attrib.has_key("to") else None),
+                parse_datestr(date.attrib["notBefore"] if date.attrib.has_key("notBefore") else None),
+                parse_datestr(date.attrib["notAfter"] if date.attrib.has_key("notAfter") else None))
 
-        if date_when is not None:
-            date_average = date_when
-        else:
-            if date_from is not None and date_to is None:
-                date_average = date_from
-            else:
-                if date_to is not None and date_from is None:
-                    date_average = date_to
-                else:
-                    if date_from is not None and date_to is not None:
-                        date_average = date_from + ((date_to - date_from) / 2)
-                    else:
-                        break
+            source_uri = date.find('f:source', namespaces=faust.namespaces).attrib['uri']
 
-        source_uri = date.find('f:source', namespaces=faust.namespaces).attrib['uri']
+            date_id = 'date_{0}'.format(date_index)
+            date_label = str(absolute_dating)
+            logging.info('Parsed Date: %s' % (date_label))
 
-        date_id = 'date_{0}'.format(date_index)
-        date_label = "{0}".format(format_date(date_when)) if date_when is not None else "{0} - {1}" \
-            .format(format_date(date_from), format_date(date_to), format_date(date_average))
-        logging.info(date_label)
+            items = date.findall('f:item', namespaces=faust.namespaces)
+            absolute_datings = []
+            for item in items:
+                # TODO normalize uris
+                item_uri = item.attrib["uri"]
+                if not item_uri in graph.node:
+                    add_item_node(graph, item_uri)
 
-        items = date.findall('f:item', namespaces=faust.namespaces)
-        for item in items:
-            item_uri = item.attrib["uri"]
-            graph.add_node(date_id, label=date_label, date_average=date_average, shape='box', color='#ffffff00')
-            graph.add_edge(date_id, item_uri, color='grey', faust_type='date')
+                absolute_datings.append(absolute_dating)
+
+            if len(absolute_datings) > 0:
+                graph.node[item_uri][KEY_ABSOLUTE_DATINGS] = absolute_datings
+                # Also pickle this because when converting to an AGraph attribute values must be strings
+                graph.node[item_uri][KEY_ABSOLUTE_DATINGS_PICKLED] = pickle.dumps(absolute_datings)
+
+        except Exception as e:
+            logging.error("Invalid absolute dating in %s" % (macrogenetic_document.docinfo.URL))
 
 
 def parse_relationships(macrogenetic_document, graph):
@@ -78,7 +141,6 @@ def parse_relationships(macrogenetic_document, graph):
     for relation in relations:
         try:
             relation_name = relation.attrib["name"]
-            edge_style = 'solid' if relation_name == 'temp-pre' else 'dashed'
             info_message = ' ' + str(relation_name) + ': '
             source_uri = relation.find('f:source', namespaces=faust.namespaces).attrib['uri']
             items = relation.findall('f:item', namespaces=faust.namespaces)
@@ -86,13 +148,25 @@ def parse_relationships(macrogenetic_document, graph):
             previous_item_uri = None
             for item in items:
                 item_uri = item.attrib["uri"]
-                graph.add_node(item_uri, label=label_from_uri(item_uri))
+                # TODO create label in visualization component
+                add_item_node(graph, item_uri)
                 if previous_item is None:
                     info_message += '   '
                 else:
+                    edge_attr_dict = {}
+                    if relation_name in GENETIC_RELATION_MAP.keys():
+                        edge_attr_dict[KEY_RELATION_NAME] = GENETIC_RELATION_MAP[relation_name]
+                    else:
+                        raise ValueError("Unknown relation {0} encountered".format(relation_name))
+
                     graph.add_edge(previous_item_uri, item_uri,  # label=label_from_uri(source_uri),
                                    # edgetooltip=str(macrogenetic_file),
-                                   style=edge_style)
+                                   attr_dict=edge_attr_dict)
+                    if relation_name == 'temp-syn':
+                        graph.add_edge(item_uri, previous_item_uri,  # label=label_from_uri(source_uri),
+                        # edgetooltip=str(macrogenetic_file),
+                        attr_dict=edge_attr_dict)
+
                     info_message += ' > '
                 info_message = info_message + ' ' + str(item_uri)
                 previous_item = item
@@ -105,10 +179,14 @@ def parse_relationships(macrogenetic_document, graph):
             # dates =  macrogenetic_document.getroot().findall('f:date', namespaces=faust.namespaces)
 
 
+def add_item_node(graph, item_uri):
+    graph.add_node(item_uri, label=label_from_uri(item_uri))
+
+
 def import_graph():
     imported_graph = networkx.MultiDiGraph()
-    imported_graph = networkx.MultiDiGraph()
-    for macrogenetic_file in faust.macrogenesis_files()[2:5]:
+    # FIXME parse whole set of files
+    for macrogenetic_file in faust.macrogenesis_files() [10:13]:#[3:6]:
         parse(macrogenetic_file, imported_graph)
 
     logging.info(
@@ -117,9 +195,9 @@ def import_graph():
 
 
 def main():
-    logging.basicConfig(level=logging.INFO)
     import_graph()
 
 
 if __name__ == '__main__':
+    logging.basicConfig(level=logging.INFO)
     main()
--- faust-utils/macrogenesis/visualize.py
+++ faust-utils/macrogenesis/visualize.py
@@ -1,58 +1,104 @@
 import logging
 import os.path
-
 import networkx
-
 import faust
 import macrogenesis
+import pickle
+
+# styles and defaults
+
+
+DEFAULT_EDGE_WEIGHT = '1'
+EDGE_STYLES = [(macrogenesis.KEY_RELATION_NAME, 'temp-pre', 'style', 'solid'),
+               (macrogenesis.KEY_RELATION_NAME, 'temp-syn', 'style', 'dashed'),
+               (macrogenesis.KEY_RELATION_NAME, macrogenesis.VALUE_IMPLICIT_FROM_ABSOLUTE, 'weight', '5'),
+               (macrogenesis.KEY_RELATION_NAME, macrogenesis.VALUE_IMPLICIT_FROM_ABSOLUTE, 'color', 'grey')]
+
+NODE_STYLES = []
+STYLE_ABSOLUTE_DATING_CLUSTER_COLOR = 'grey'
+STYLE_ABSOLUTE_DATING_COLOR = '#ffffff00'
+
+
+def apply_agraph_styles(agraph):
+    for edge in agraph.edges():
+        edge.attr['weight'] = DEFAULT_EDGE_WEIGHT
+        for (genetic_key, genetic_value, style_key, style_value) in EDGE_STYLES:
+            if genetic_key in edge.attr.keys() and edge.attr[genetic_key] == genetic_value:
+                edge.attr[style_key] = style_value
+
+    for node in agraph.nodes():
+        for (genetic_key, genetic_value, style_key, style_value) in NODE_STYLES:
+            if genetic_key in node.attr.keys() and node.attr[genetic_key] == genetic_value:
+                node.attr[style_key] = style_value
 
 
 def agraph_from(graph):
+    # append_absolute_date_nodes(graph)
+
     logging.info(" Generating agraph.")
+
     agraph = networkx.nx_agraph.to_agraph(graph)
-    #agraph.graph_attr['overlap'] = "1"
-    cluster_index = 0
 
-    dated_nodes = []
+    # group_absolute_dating_nodes_and_items(agraph)
 
-    for edge in agraph.edges():
-        edge.attr['weight'] = '0.5'
-#        print edge.attr
-#        print edge
-        if edge.attr['faust_type'] == 'date':
-            logging.debug("Processing date edge {0}".format(edge))
+    # agraph.graph_attr['overlap'] = "1"
+    # agraph.graph_attr['concentrate'] = "true"
 
-            date = edge[0]
-            dated_node = edge[1]
-            cluster_id = 'cluster{0}'.format(cluster_index)
-            agraph.add_subgraph(nbunch=[date, dated_node], name= cluster_id, color='blue')
-            cluster_index += 1
-            agraph.remove_edge(edge)
-            dated_nodes.append((date, dated_node))
+    visualize_absolute_datings_2(agraph)
 
+    apply_agraph_styles(agraph)
 
-    logging.debug("Sorting dates")
-    dated_nodes.sort(key=lambda e: e[0].attr['date_average'])
-    previous_dated_node = None
-    for edge in dated_nodes:
-        if previous_dated_node is not None:
-            agraph.add_edge(previous_dated_node, edge[1], color='grey')
-        previous_dated_node = edge[1]
     return agraph
 
+
+def visualize_absolute_datings_2(agraph):
+    cluster_index = 0
+    absolute_dating_index = 0
+    dated_nodes = []
+
+    for node in agraph.nodes():
+        if macrogenesis.KEY_ABSOLUTE_DATINGS_PICKLED in node.attr.keys():
+            logging.debug("Adding cluster for absolute datings of node {0}".format(node))
+            absolute_datings = pickle.loads(node.attr[macrogenesis.KEY_ABSOLUTE_DATINGS_PICKLED])
+            absolute_dating_nodes = []
+            for absolute_dating in absolute_datings:
+                date_id = 'date_{0}'.format(absolute_dating_index)
+                date_label = str(absolute_dating)
+                agraph.add_node(date_id, label=date_label, date_average=absolute_dating.average, shape='box',
+                                color=STYLE_ABSOLUTE_DATING_COLOR)
+                absolute_dating_nodes.append(date_id)
+                absolute_dating_index += 1
+            cluster_id = 'cluster{0}'.format(cluster_index)
+            agraph.add_subgraph(nbunch=absolute_dating_nodes + [node], name=cluster_id,
+                                color=STYLE_ABSOLUTE_DATING_CLUSTER_COLOR)
+            cluster_index += 1
+
+
 def main():
-    logging.basicConfig(level=logging.DEBUG)
     output_dir = faust.config.get("macrogenesis", "output-dir")
 
     # draw raw input data
-    imported_graph = macrogenesis.import_graph()
-    agraph_imported_graph = agraph_from(imported_graph)
+    graph_imported = macrogenesis.import_graph()
+    agraph_imported = agraph_from(graph_imported)
 
     logging.info("Generating raw data graph.")
 
-    agraph_imported_graph.layout(prog='dot')
-    agraph_imported_graph.draw(os.path.join(output_dir, 'raw_data.svg'))
-    agraph_imported_graph.write(os.path.join(output_dir, 'raw_data.dot'))
+    agraph_imported.layout(prog='dot')
+    agraph_imported.draw(os.path.join(output_dir, '00_raw_data.svg'))
+    agraph_imported.write(os.path.join(output_dir, '00_raw_data.dot'))
+
+    # add relationships implicit in absolute datings
+    logging.info("Generating graph with implicit absolute date relationships.")
+
+    graph_absolute_edges = graph_imported
+    macrogenesis.insert_minimal_edges_from_absolute_datings(graph_imported)
+    # del imported_graph
+
+    agraph_absolute_edges = agraph_from(graph_imported)
+
+    agraph_absolute_edges.layout(prog='dot')
+    agraph_absolute_edges.draw(os.path.join(output_dir, '10_absolute_edges.svg'))
+    agraph_absolute_edges.write(os.path.join(output_dir, '10_absolute_edges.dot'))
 
     # highlight a single node and its neighbors
     # highlighted_node = 'faust://document/wa/2_I_H.17'
@@ -61,12 +107,12 @@ def main():
     # highlighted_subgraph = imported_graph.subgraph(nbunch=highlighted_bunch)
     # agraph_highlighted_subgraph = networkx.nx_agraph.to_agraph(highlighted_subgraph)
     # agraph_highlighted_subgraph.layout(prog='dot')
-    # agraph_highlighted_subgraph.draw(os.path.join(output_dir, 'highlighted.svg'))
-    # agraph_highlighted_subgraph.write(os.path.join(output_dir, 'highlighted.dot'))
+    # agraph_highlighted_subgraph.draw(os.path.join(output_dir, '20_highlighted.svg'))
+    # agraph_highlighted_subgraph.write(os.path.join(output_dir, '20_highlighted.dot'))
 
-    # transitive closure
+    # transitive closure, don't draw
     logging.info("Generating transitive closure graph.")
-    transitive_closure = networkx.transitive_closure(imported_graph)
+    transitive_closure = networkx.transitive_closure(graph_imported)
     logging.info("{0} nodes, {1} edges in transtive closure.".format(transitive_closure.number_of_nodes(),
                                                                      transitive_closure.number_of_edges()))
     agraph_transitive_closure = agraph_from(transitive_closure)
@@ -74,10 +120,12 @@ def main():
     # draw transitive reduction
     logging.info("Generating transitive reduction graph.")
     agraph_transitive_reduction = agraph_transitive_closure.tred(copy=True)
-    logging.info("{0} nodes, {1} edges in transtive reduction.".format(agraph_transitive_reduction.number_of_nodes(),                                                                       agraph_transitive_reduction.number_of_edges()))
+    logging.info("{0} nodes, {1} edges in transtive reduction.".format(agraph_transitive_reduction.number_of_nodes(),
+                                                                       agraph_transitive_reduction.number_of_edges()))
     agraph_transitive_reduction.layout(prog='dot')
-    agraph_transitive_reduction.draw(os.path.join(output_dir, 'transitive_reduction.svg'))
+    agraph_transitive_reduction.draw(os.path.join(output_dir, '30_transitive_reduction.svg'))
 
 
 if __name__ == '__main__':
+    logging.basicConfig(level=logging.INFO)
     main()
