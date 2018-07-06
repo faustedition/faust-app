ec98c5f7ddbebeb121e76938d7b1150fd2891b92
Thorsten Vitt <thorsten.vitt@uni-wuerzburg.de>
2018-07-06 15:22:59 +0200

    Tests and ambiguous uri stuff

--- faust-utils/macrogenesis/uris.py
+++ faust-utils/macrogenesis/uris.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+import codecs
 import csv
 import json
 import logging
@@ -14,12 +16,13 @@ class Inscription(object):
     def __init__(self, witness, inscription):
         self.witness = witness
         self.inscription = inscription
-        self.known_inscription = inscription in witness.inscriptions
         self.known_witness = isinstance(witness, Witness)
-        if self.known_inscription and self.known_witness:
-            self.status = 'ok'
-        elif self.known_witness:
-            self.status = 'unknown-inscription'
+        if self.known_witness:
+            self.known_inscription = inscription in getattr(witness, 'inscriptions', {})
+            if self.known_inscription:
+                self.status = 'ok'
+            else:
+                self.status = 'unknown-inscription'
         else:
             self.status = 'unknown'
 
@@ -54,7 +57,34 @@ class UnknownRef(object):
         if isinstance(other, UnknownRef):
             return self.uri == other.uri
         else:
-            return str(self) == str(other)
+            return self is other
+
+
+class AmbiguousRef(object):
+
+    def __init__(self, uri, wits):
+        self.uri = uri
+        self.witnesses = frozenset(wits)
+        self.status = 'ambiguous: ' + ", ".join(str(wit) for wit in sorted(self.witnesses))
+
+    def __hash__(self):
+        return hash(self.uri)
+
+    def __eq__(self, other):
+        if isinstance(other, AmbiguousRef):
+            return self.uri == other.uri
+        return self is other
+
+    def first(self):
+        return sorted(self.witnesses, key=str)[0]
+
+    def __add__(self, other):
+        new_witnesses = {other} if isinstance(other, Witness) else set(other.witnesses)
+        new_witnesses = new_witnesses.union(self.witnesses)
+        return AmbiguousRef(self.uri, new_witnesses)
+
+    def __str__(self):
+        return self.uri
 
 
 class Witness(object):
@@ -68,12 +98,15 @@ class Witness(object):
             raise TypeError('doc_record must be a mapping, not a ' + str(type(doc_record)))
 
     def uris(self):
-        yield self.uri
+        result = [self.uri]
         if hasattr(self, 'other_sigils'):
             for uri in self.other_sigils:
-                yield uri
+                result.append(uri)
                 if u'/wa_faust/' in uri:
-                    yield uri.replace('/wa_faust/', '/wa/')
+                    result.append(uri.replace('/wa_faust/', '/wa/'))
+        if getattr(self, 'type', '') == 'print':
+            result.extend([uri.replace('faust://document/', 'faust://print/') for uri in result])
+        return result
 
     @classmethod
     def _load_database(cls,
@@ -81,31 +114,44 @@ class Witness(object):
         sigil_json = urllib2.urlopen(url)
         sigil_data = json.load(sigil_json)
         sigil_json.close()
+        cls.database = cls.build_database(sigil_data)
+
+    @classmethod
+    def build_database(cls, sigil_data):
+        database = {}
         for doc in sigil_data:
             wit = cls(doc)
             for uri in wit.uris():
-                cls.database[uri] = wit
-
-        for ref, uri in sorted(cls.database.items()):
-            print ref, uri
+                if uri in database:
+                    old_entry = database[uri]
+                    logging.warning("URI %s is already in db for %s, adding %s", uri, old_entry, wit)
+                    if isinstance(old_entry, AmbiguousRef):
+                        database[uri] = old_entry + wit
+                    else:
+                        database[uri] = AmbiguousRef(uri, {database[uri], wit})
+                else:
+                    database[uri] = wit
+        return database
 
     @classmethod
-    def get(cls, uri):
+    def get(cls, uri, allow_duplicate=False):
         if not cls.database:
             cls._load_database()
 
         if uri in cls.database:
-            return cls.database[uri]
+            result = cls.database[uri]
+            if isinstance(result, AmbiguousRef):
+                return result if allow_duplicate else result.first()
+            else:
+                return result
 
         if uri.startswith('faust://inscription'):
-            try:
-                system, sigil, inscription = re.match('^faust://inscription/(.*)/(.*)/(.*)', uri).groups()
-                base = "/".join(['faust://document', system, inscription])
-                if base in cls.database:
-                    wit = cls.database[base]
-                    return Inscription(wit, inscription)
-            except AttributeError:
-                logging.error("Broken inscription uri: %s", uri)
+            match = re.match('faust://inscription/(.*)/(.*)/(.*)', uri)
+            if match is not None:
+                system, sigil, inscription = match.groups()
+                base = "/".join(['faust://document', system, sigil])
+                wit = cls.get(base)
+                return Inscription(wit, inscription)
 
         logging.warning('Unknown reference: %s', uri)
         return UnknownRef(uri)
@@ -119,7 +165,7 @@ class Witness(object):
         return str(self) == str(other)
 
     def __str__(self):
-        return self.uri
+        return self.sigil_t
 
 
 def _collect_wits():
@@ -129,27 +175,28 @@ def _collect_wits():
         for element in tree.xpath('//f:item', namespaces=faust.namespaces):  # type: etree._Element
             uri = element.get('uri')
             wit = Witness.get(uri)
-            items[wit].append((macrogenetic_file, element.sourceline))
+            items[wit].append((macrogenetic_file.split('macrogenesis/')[-1], element.sourceline))
     return items
 
 
 def _report_wits(wits, output_csv='witness-usage.csv'):
-    with open(output_csv, "wt") as reportfile:
+    with codecs.open(output_csv, "wt", encoding='utf-8') as reportfile:
         table = csv.writer(reportfile)
         table.writerow(['URI', 'Status', 'Vorkommen'])
         for wit in sorted(wits, key=str):
             table.writerow([
-                wit.uri,
+                str(wit),
                 wit.status,
                 ", ".join([file + ":" + str(line) for file, line in wits[wit]])
             ])
-        for wit in Witness.database.values():
+        for wit in sorted(set(Witness.database.values()), key=str):
             if wit not in wits:
                 table.writerow([
-                    wit.uri,
+                    str(wit),
                     "no-data",
                     ""
                 ])
+
 
 if __name__ == '__main__':
     wits = _collect_wits()
